9dcd731a4e3b09cf54ec29a2b64b802e6293fdcf
Letsktor <vamat@mailbox.unideb.hu>
2024-02-29 13:56:52 +0100

    Implemented a qr-code reader to get the number fo the card

--- app/src/main/java/hu/undieb/nyilvantarto/MainActivity.java
+++ app/src/main/java/hu/undieb/nyilvantarto/MainActivity.java
@@ -8,19 +8,21 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 
-import android.nfc.FormatException;
-import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
 
 import android.nfc.Tag;
 
-import android.nfc.tech.Ndef;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.IOException;
+import com.google.mlkit.vision.barcode.common.Barcode;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanner;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -29,12 +31,16 @@ public class MainActivity extends AppCompatActivity {
     NfcAdapter nfcAdapter;
     Intent intent;
     PendingIntent pendingIntent;
+    ImageView QrCodeScanner;
+    TextView txtBArCOdeValue;
     Tag tag;
     int xd=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtBArCOdeValue=findViewById(R.id.txtFromQrCode);
+        QrCodeScanner=findViewById(R.id.btnQrCodeScanner);
         nfcAdapter=NfcAdapter.getDefaultAdapter(this);
         intent=new Intent(this,getClass());
         intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
@@ -45,6 +51,35 @@ public class MainActivity extends AppCompatActivity {
         {
             Toast.makeText(this,tag.toString(),Toast.LENGTH_LONG).show();
         }
+        GmsBarcodeScannerOptions options = new GmsBarcodeScannerOptions.Builder()
+                .setBarcodeFormats(
+                        Barcode.FORMAT_QR_CODE,
+                        Barcode.FORMAT_AZTEC)
+                .build();
+        GmsBarcodeScanner scanner = GmsBarcodeScanning.getClient(this);
+        QrCodeScanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scanner
+                        .startScan()
+                        .addOnSuccessListener(
+                                barcode -> {
+                                    String rawValue = barcode.getRawValue();
+                                    String[] temp=rawValue.split("/");
+                                    txtBArCOdeValue.setText(temp[temp.length-1]);
+                                })
+                        .addOnCanceledListener(
+                                () -> {
+                                    // Task canceled
+                                })
+                        .addOnFailureListener(
+                                e -> {
+                                    // Task failed with an exception
+                                });
+            }
+
+        });
+
     }
     @Override
     protected void onNewIntent(Intent intent) {
