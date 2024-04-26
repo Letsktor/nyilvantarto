6860f20b9ea7f52c52c2362b4ea6eb9b4439bd24
Letsktor <vamat@mailbox.unideb.hu>
2024-04-26 11:59:08 +0200

    Created the KurzusokRecViewAdapter java file.

---
+++ app/src/main/java/hu/undieb/nyilvantarto/KurzusokRecViewAdapter.java
@@ -0,0 +1,30 @@
+package hu.undieb.nyilvantarto;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class KurzusokRecViewAdapter extends RecyclerView.Adapter<KurzusokRecViewAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public KurzusokRecViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull KurzusokRecViewAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
