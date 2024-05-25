f2843118874c004a3f3edc40d9e9c24b48fb2b04
Máté Vajas <vajasmate2013@gmail.com>
2024-05-25 11:22:27 +0200

    Made a slight modification in the database

--- app/src/main/java/hu/undieb/nyilvantarto/KurzusokUtils.java
+++ app/src/main/java/hu/undieb/nyilvantarto/KurzusokUtils.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -15,7 +16,7 @@ import java.util.ArrayList;
 public class KurzusokUtils {
     private static volatile KurzusokUtils instance;
     private LiveData<ArrayList<Kurzus>> kurzusok;
-    private DatabaseReference database= FirebaseDatabase.getInstance().getReference("Kurzusok");
+    private DatabaseReference database= FirebaseDatabase.getInstance().getReference(FirebaseAuth.getInstance().getUid()).child("Kurzusok");
     private KurzusokUtils(){
         if (null==kurzusok)
         {
